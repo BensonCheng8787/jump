6638e293fcb453f294f3ada5a5c7da66975f08bb
BensonCheng8787 <bensoncheng888@gmail.com>
2023-09-22 17:12:49 -0400

    initial debugging to try to fix the pop up window

--- popup.js
+++ popup.js
@@ -1,30 +1,26 @@
 document.getElementById("startGame").addEventListener("click", () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const activeTab = tabs[0];
-      chrome.scripting.executeScript({
-        target: { tabId: activeTab.id },
-        function: startGame,
-      });
-    });
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const activeTab = tabs[0];
+    startGame(activeTab); // Pass activeTab as a parameter to startGame
   });
-  
-  function startGame() {
-    chrome.scripting.insertCSS({
-      target: { tabId: activeTab.id },
-      css: "body { background: transparent; }",
-    });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: activeTab.id },
-      function: () => {
-        // Inject the game by creating a <div> with game.html content.
-        const gameDiv = document.createElement("div");
-        gameDiv.id = "jumpingGame";
-        gameDiv.innerHTML = `
-          <iframe src="${chrome.runtime.getURL("game.html")}" frameborder="0"></iframe>
-        `;
-        document.body.appendChild(gameDiv);
-      },
-    });
-  }
-  
+});
+
+function startGame(activeTab) {
+  chrome.scripting.insertCSS({
+    target: { tabId: activeTab.id },
+    css: "body { background: transparent; }",
+  });
+
+  chrome.scripting.executeScript({
+    target: { tabId: activeTab.id },
+    function: () => {
+      // Inject the game by creating a <div> with game.html content.
+      const gameDiv = document.createElement("div");
+      gameDiv.id = "jumpingGame";
+      gameDiv.innerHTML = `
+        <iframe src="${chrome.runtime.getURL("game.html")}" frameborder="0"></iframe>
+      `;
+      document.body.appendChild(gameDiv);
+    },
+  });
+}
